a55f9d94f2a7ff752f2a231968eb61d76889e8d1
Scott Allen <scott-allen@lambdastudents.com>
2020-09-15 17:31:24 -0600

    Finished with all the tasks

--- index.js
+++ index.js
@@ -82,11 +82,11 @@ Parameters:
  * callback function getYears
  */
 
-function getWinnersByYear(/* code here */) {
-
+function getWinnersByYear(cbFunc1, cbFunc2) {
+    return `In ${cbFunc1[0]}, ${cbFunc2[0]} won the world cup!`;
 };
 
-getWinnersByYear();
+console.log(getWinnersByYear(getYears(getFinals(fifaData)), getWinners(getFinals(fifaData))));
 
 /* Task 6: Write a function called `getAverageGoals` that accepts a parameter `data` and returns the the average number of home team goals and away team goals scored per match (Hint: use .reduce and do this in 2 steps) */
 
